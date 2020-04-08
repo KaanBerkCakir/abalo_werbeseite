55267e12725dde81e2ce21b12d5b7e6a923ab7cd
Sanoj3008 <jonas.schell@gmx.de>
2020-04-08 15:36:55 +0200

    new style / overview added

---
+++ html/includes/toolbar.php
@@ -0,0 +1,7 @@
+<div id="navbar" class="row al-end">
+    <div id="title" class="grow-1 column jc-end">
+        <span>Abalo</span>
+        <span>Macht Altes Flüssig.</span>
+    </div>
+    <input type="text" placeholder="Suche...">
+</div>
