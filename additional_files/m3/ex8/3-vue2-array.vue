742a8381a6977f3dac7b82378a04f8f0d37775e7
KaanBerkCakir <kaan-berk.cakir@alumni.fh-aachen.de>
2020-05-15 14:53:13 +0200

    Ex 8 32 compledet

--- additional_files/m3/ex8/3-vue2-array.vue
+++ additional_files/m3/ex8/3-vue2-array.vue
@@ -10,15 +10,41 @@
 </head>
 <body>
 <div id="app">
+    <button v-on:click="addRadio">
+    UKW-Radio adden
+    </button>
 
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Preis</th>
+        </tr>
+        <tr v-for="item in items">
+            <td>{{item.name}}</td>
+            <td>{{item.price}}</td>
+        </tr>
+    </table>
 </div>
 <script>
-    new Vue({
-        el: '#app',
-        data: {
+   let app =  new Vue({
+       el: '#app',
+       data: {
+           items: [
+               {name: "Reifen", price: 8290},
+               {name: "Antenne drehbar. Leicht geknickt", price: 1400},
+               {name: "Opel Kadett C 1992 – wie neu", price: 150000},
+           ]
+       },
+       methods: {
+           addRadio: function () {
+               this.items.push(
+                   {name: "UKW-Radio", price: 45}
+               )
+           }
+       }
+   });
 
-        }
-    });
 </script>
+
 </body>
 </html>
